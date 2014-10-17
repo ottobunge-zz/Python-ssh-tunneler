98f8b45afc57c61290d81576810597dfe58cfa11
Octavio <octavio@ottomundo.com.ar>
2014-10-17 06:03:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+Python-ssh-tunneler
+===================
+
+Little script to create, detect and kill local ssh tunnels
+
+Setup:
+======
+
+create an object with:
+
+*localport (port tunneling from, local side)
+
+*remoteport (port tunneling to, server side)
+
+*sshport (port ssh is runnin on, server machine, defaul = 22)
+
+*host (remote host)
+
+*user (user on remote host)
+
+*idfile (private key user uses to id in remote host)
+
+
+Available functions:
+====================
+
+*makeTunnel(), creates desired tunnel, returns tunnel process id, or false, depending on success of creation
+
+*checkTunnel(), returns tunnel process id, or false, depending if tunnel exists or not
+
+*killTunnel(), kills current tunnel, returns tunnel process id, or false, depending on success of deletion
+
+*checkOpenPort(): Checks if local port is being used or not, returns True or False, if used or not
+
+both killtunnel and maketunnel check first before making the tunnel
+
+example: ssh = Tunnel(localport,remoteport,sshport,host,user,idfile)
---
+++ tunneler.py
@@ -0,0 +1,56 @@
+import commands, os, time
+class Tunnel:
+	def __init__(self, localport, remoteport, sshport, host, user,idfile):
+		self.localport = localport
+		self.remoteport = remoteport
+		self.sshport = sshport
+		self.host = host
+		self.user = user
+		self.idfile = idfile
+		self.command = 'ssh -f ' + self.user+'@'+self.host + ' -L ' + self.localport + ':' + self.host + ':' + self.remoteport + ' -N -p ' + self.sshport + ' -i ' + self.idfile
+	def checkOpenPort(self):
+		portcheck = 'netstat -a | grep :' + self.localport
+		portcheckout = commands.getoutput(portcheck)
+		if self.localport in portcheckout:
+			return True
+		else:
+			return False
+
+	def checkTunnel(self):
+		isAlive = commands.getoutput('ps aux')
+		if self.checkOpenPort() and self.command.replace(self.idfile,'') not in isAlive:
+			print 'Port ', self.localport, ' already in use, please use another one!'
+			return True
+		else:		
+			if self.command.replace(self.idfile,'') in isAlive:
+				self.tunnelid = commands.getoutput("ps aux |  grep \"" + self.command.replace(self.idfile,'') + "\" | awk '{print $2}' |head -1")
+				return self.tunnelid
+			else:
+				return False
+		
+	def makeTunnel(self):
+		self.check = self.checkTunnel()
+		if self.check:
+			if self.check == True:
+				return False
+			else:
+				print 'Tunnel Already exists! with process id:', self.check
+				return False	
+		else:
+			os.system(self.command)
+			if self.checkTunnel():
+				return True
+			else:
+				return False
+
+	def killTunnel(self):
+		if self.checkTunnel():
+			os.system('kill -9 ' + self.tunnelid)
+			if self.checkTunnel():
+				return False				
+			else:
+				return True
+		else:
+			return False
+
+
